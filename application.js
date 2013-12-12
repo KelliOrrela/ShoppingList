d7386f7e24710b27ebcacf5f4ada6f27c44a0f70
KelliOrrela <kelli.orrela@gmail.com>
2013-12-12 12:12:59 +0200

    changed UI somewhat and added basic functionality

---
+++ application.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+
+var item;
+
+//Input with enter key
+$("#newitem").keyup(function(e) {
+  if (e.which == 13) {
+    e.preventDefault();
+    addItem();
+  }
+});
+
+//Add item
+function addItem() {
+  var item = document.getElementById("newitem").value;
+  $(".addedlist").append("<li>" + item + "</li>");
+  $("#newitem").val("");
+  $("#newitem").focus();
+}
+
+//Strike through added item
+$(".addedlist li").click(function() {
+  $(this).addClass("strikethrough");
+});
+
+//Delete removed item
+$(".removedlist li").click(function() {
+  $(this).remove();
+});
+
+
+});
