7711cf13c809a9b66835f1613f8af168968b99e2
KelliOrrela <kelli.orrela@gmail.com>
2013-12-26 10:00:14 +0200

    revised UI for improved readability and clarity

--- application.js
+++ application.js
@@ -18,11 +18,12 @@ $("#newitem").focus();
 //Add item
 function addItem() {
   var item = document.getElementById("newitem").value;
+  //Prevent user from not entering any input or entering only space
   if (item == "" ||  item == " ") {
     return(false);
   }
   else {
-  $(".list").append("<li><input type='button' class='checkitem' value='&#x2713;'/>" + item + "<input type='button' class='removeitem' value='&#x2715;'/><span id='sortitems'>&#8597;</span></li>");
+  $(".list").append("<li><span id='sortitems'>&#8597;</span><input type='button' class='checkitem' value='&#x2713;'/>" + item + "<input type='button' class='removeitem' value='&#x2715;'/></li>");
   //Reset placeholder text and return focus to input field
   $("#newitem").val("");
   $("#newitem").focus();
@@ -42,12 +43,7 @@ $(".list").on("click", ".removeitem", function(e) {
 });
 
 //Make list items sortable
-$(".list").sortable({axis: "y", containment: ".list", revert: true, scroll: true, scrollspeed: 40, tolerance: "pointer",
-  //Control width of item when being sorted
-  start: function(event, ui) {
-  $(ui.item).width($(".listcontainer").width());
-  $("#newitem").focus();
-  }});
+$(".list").sortable({axis: "y", containment: ".list", cursor: "move", revert: 250, scroll: true, scrollspeed: 20, tolerance: "pointer"});
 
 
 });
